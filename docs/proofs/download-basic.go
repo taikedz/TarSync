367045fb2172c411490f459fb2d2d197d401660d
Tai Kedzierski <dch.tai@gmail.com>
2024-12-06 14:06:02 +0000

    Clarify the download proof code

--- docs/proofs/download-basic.go
+++ docs/proofs/download-basic.go
@@ -8,14 +8,18 @@ import (
 
 func download(number int, url string, wg *sync.WaitGroup, back chan string) {
 	defer wg.Done()
+	fmt.Println("Starting download ... ", url)
 	if err := net.FetchHttp(url, fmt.Sprintf("scratch/file%d.html", number)); err != nil {
-		back <- fmt.Sprintf("Failed: %s", url)
+		back <- fmt.Sprintf("Failed: %s -> %s", url, err)
+		return
 	}
+	fmt.Println("End download ", url)
 }
 
 func main() {
 	links := []string{"https://www.example.com", "https://dev.to", "https://www.example.nope"}
 	
+	// declaring the variable with the struct type initializes a memory space with its zeroed values
     var wg sync.WaitGroup
 	wg.Add(len(links))
     messages := make(chan string, len(links))
